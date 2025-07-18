65a00a5ed088f4b0d8821e89c25c20a9398ea5db
james-paul25 <jamespaul.dacaldacal25@gmail.com>
2025-07-18 19:42:01 +0800

    added chaviron icon on chat card mga maennn

--- src/components/ChatCard.jsx
+++ src/components/ChatCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Avatar from "../assets/prof.jpg";
-
+import { ChevronRight } from "lucide-react";
 
 const ChatCard = ({ session, profilePictures, onClick }) => {
     
@@ -15,23 +15,27 @@ const ChatCard = ({ session, profilePictures, onClick }) => {
 
     return (
         <>
-        <div
-            onClick={onClick}
-            className={`bg-white hover:bg-gray-50 shadow rounded-lg p-4 flex gap-4 cursor-pointer`}
-            >
-            <img
+       <div
+        onClick={onClick}
+        className="bg-white hover:bg-gray-50 shadow rounded-lg p-4 flex items-center justify-between cursor-pointer"
+        >
+            <div className="flex items-center gap-4">
+                <img
                 src={imageUrl}
                 alt={session?.tutor?.student?.fullName || "Tutor Avatar"}
                 className="w-12 h-12 rounded-full"
-            />
-            <div>
+                />
+                <div>
                 <h3 className="font-semibold text-purple-900">
-                {session?.tutor?.student?.fullName}
+                    {session?.tutor?.student?.fullName}
                 </h3>
-                <p className="font text-sm text-gray-500">
+                <p className="text-sm text-gray-500">
                     <span>message here</span>
                 </p>
+                </div>
             </div>
+
+            <ChevronRight className="text-gray-400" />
         </div>
         </>
     );
