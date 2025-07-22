70471780a6ceff03b03499ef76dfd97d2c6918b3
james-paul25 <jamespaul.dacaldacal25@gmail.com>
2025-07-22 12:22:21 +0800

    added updateSession function

--- src/services/sessionService.js
+++ src/services/sessionService.js
@@ -1,37 +1,55 @@
 const getSessionByStudent = async (studentId) => {
-    const res = await fetch(`http://localhost:8080/api/v1/sessions/get-session-by-student/${studentId}`);
+  const res = await fetch(`http://localhost:8080/api/v1/sessions/get-session-by-student/${studentId}`);
 
-    if (res.status === 404) {
-        return [];
-    }
+  if (res.status === 404) {
+    return [];
+  }
 
-    if (!res.ok) throw new Error("Failed to fetch sessions");
-    return res.json();
+  if (!res.ok) throw new Error("Failed to fetch sessions");
+  return res.json();
 }
 
 const fetchSessionByTutor = async (tutorId) => {
-    const res = await fetch(`http://localhost:8080/api/v1/sessions/get-session-by-tutor/${tutorId}`);
+  const res = await fetch(`http://localhost:8080/api/v1/sessions/get-session-by-tutor/${tutorId}`);
 
-    if (res.status === 404) {
-        return [];
-    }
+  if (res.status === 404) {
+    return [];
+  }
 
-    if (!res.ok) throw new Error("Failed to fetch session.");
-    return res.json();
+  if (!res.ok) throw new Error("Failed to fetch session.");
+  return res.json();
 }
 
 const setStatusComplete = async ({ sessionId }) => {
-    try {
-      const response = await fetch(`http://localhost:8080/api/v1/sessions/update-status-completed/${sessionId}`, {
-        method: 'PUT',
-        credentials: "include",
-      });
-  
-      return response.text();
-    } catch (error) {
-      return error.message;
-    }
-  };
-  
+  try {
+    const response = await fetch(`http://localhost:8080/api/v1/sessions/update-status-completed/${sessionId}`, {
+      method: 'PUT',
+      credentials: "include",
+    });
 
-export { getSessionByStudent, fetchSessionByTutor, setStatusComplete };
+    return response.text();
+  } catch (error) {
+    return error.message;
+  }
+};
+
+const updateSessionStatus = async ({ sessionId, sessionStatus }) => {
+  try {
+    const res = await fetch(`http://localhost:8080/api/v1/sessions/update-status/${sessionId}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+      body: JSON.stringify({ status: sessionStatus }),
+    });
+
+    if (!res.ok) throw new Error("Error approving session.");
+
+    return await res.json();
+
+  } catch (e) {
+    return e;
+  }
+}
+
+
+export { getSessionByStudent, fetchSessionByTutor, setStatusComplete, updateSessionStatus };
